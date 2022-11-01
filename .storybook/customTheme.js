4fa0089cf333683175213add22f57ce61dc2631f
FZChlo <fzchlo@gmail.com>
2022-11-01 14:47:28 +0100

    Quick clean up

--- .storybook/customTheme.js
+++ .storybook/customTheme.js
@@ -3,6 +3,5 @@ import { create } from "@storybook/theming";
 export default create({
 	base: "dark",
 	brandTitle: "React Light Modal",
-	brandUrl: "https://reactlightmodal.dame.vin",
 	brandImage: "https://user-images.githubusercontent.com/72107589/149128610-e2e79c72-2df6-4315-b632-1a71c5ef84f4.png",
 });
